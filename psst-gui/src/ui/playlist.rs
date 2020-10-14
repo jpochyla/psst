2ad3402f5deb3abfda13d781d561ebf9bea83043
Jan Pochyla <jpochyla@gmail.com>
2020-10-14 20:38:50 +0200

    gui: tweak the playlist padding a little

--- psst-gui/src/ui/playlist.rs
+++ psst-gui/src/ui/playlist.rs
@@ -20,7 +20,7 @@ pub fn make_list() -> impl Widget<State> {
                 .with_line_break_mode(LineBreaking::WordWrap)
                 .with_text_size(theme::TEXT_SIZE_SMALL)
                 .expand_width()
-                .padding(Insets::uniform_xy(theme::grid(2.0), theme::grid(0.5)))
+                .padding(Insets::uniform_xy(theme::grid(2.0), theme::grid(0.6)))
                 .hover()
                 .on_click(|ctx, playlist, _| {
                     let nav = Navigation::PlaylistDetail(playlist.clone());
