0a4da8cb26de0b0a69f758292308fca76d03949e
Jan Pochyla <jpochyla@gmail.com>
2020-10-22 10:43:48 +0200

    gui: improve logging

--- psst-gui/src/main.rs
+++ psst-gui/src/main.rs
@@ -15,9 +15,20 @@ use crate::{
     delegate::Delegate,
 };
 use druid::{AppLauncher, WindowDesc};
+use env_logger::{Builder, Env};
+
+const ENV_LOG: &str = "PSST_LOG";
+const ENV_LOG_STYLE: &str = "PSST_LOG_STYLE";
 
 fn main() {
-    env_logger::init();
+    // Setup logging from the env variables, with defaults.
+    Builder::from_env(
+        Env::new()
+            // `ureq` is a bit too noisy, log only warnings by default.
+            .filter_or(ENV_LOG, "info,ureq::unit=warn")
+            .write_style(ENV_LOG_STYLE),
+    )
+    .init();
 
     let main_window = WindowDesc::new(ui::make_root)
         .title("Psst")
