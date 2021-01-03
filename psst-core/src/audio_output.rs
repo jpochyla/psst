f2df55affd79ee253f53bb6b4af89e6e6aad497d
Jan Pochyla <jpochyla@gmail.com>
2021-01-03 14:59:47 +0100

    core: Experiment with miniaudio backend

--- psst-core/src/audio_output.rs
+++ psst-core/src/audio_output.rs
@@ -1,9 +1,7 @@
 use crate::error::Error;
 use crossbeam_channel::{unbounded, Receiver, Sender};
-use std::{
-    convert::TryInto,
-    sync::{Arc, Mutex},
-};
+use miniaudio::{Device, DeviceConfig, DeviceType, Format};
+use std::sync::{Arc, Mutex};
 
 pub type AudioSample = i16;
 
@@ -13,7 +11,6 @@ pub trait AudioSource: Iterator<Item = AudioSample> {
 }
 
 pub struct AudioOutputRemote {
-    ctx: Arc<soundio::Context>,
     event_sender: Sender<InternalEvent>,
 }
 
@@ -32,49 +29,19 @@ impl AudioOutputRemote {
 
     fn send(&self, event: InternalEvent) {
         self.event_sender.send(event).expect("Audio output died");
-        self.ctx.wakeup();
     }
 }
 
 pub struct AudioOutput {
-    ctx: Arc<soundio::Context>,
     event_sender: Sender<InternalEvent>,
     event_receiver: Receiver<InternalEvent>,
 }
-
-const WRITE_CALLBACK_LATENCY_SEC: f64 = 0.025;
 
 impl AudioOutput {
     pub fn open() -> Result<Self, Error> {
         let (event_sender, event_receiver) = unbounded();
 
-        let error_callback = {
-            let event_sender = event_sender.clone();
-            move |err| {
-                event_sender
-                    .send(InternalEvent::Error(err))
-                    .expect("Failed to send AudioOutputEvent::Error")
-            }
-        };
-        let dev_callback = {
-            let event_sender = event_sender.clone();
-            move || {
-                event_sender
-                    .send(InternalEvent::DevicesChanged)
-                    .expect("Failed to send AudioOutputEvent::DevicesChanged")
-            }
-        };
-
-        let mut ctx = soundio::Context::new_with_callbacks(
-            Some(error_callback),
-            Some(dev_callback),
-            None::<fn()>,
-        );
-        ctx.set_app_name("Psst");
-        ctx.connect()?;
-
         Ok(Self {
-            ctx: Arc::new(ctx),
             event_sender,
             event_receiver,
         })
@@ -82,120 +49,73 @@ impl AudioOutput {
 
     pub fn remote(&self) -> AudioOutputRemote {
         AudioOutputRemote {
-            ctx: self.ctx.clone(),
             event_sender: self.event_sender.clone(),
         }
     }
 
     pub fn start_playback<T>(&self, source: Arc<Mutex<T>>) -> Result<(), Error>
     where
-        T: AudioSource,
+        T: AudioSource + Send + 'static,
     {
-        let write_callback = {
-            let source = source.clone();
-            move |out: &mut soundio::OutStreamWriter| {
-                let mut source = source.lock().expect("Failed to acquire audio source lock");
-                out.begin_write(out.frame_count_max())
-                    .expect("Failed to begin writing the audio output stream");
-                for f in 0..out.frame_count() {
-                    for c in 0..out.channel_count() {
-                        let silence = 0;
-                        out.set_sample(c, f, source.next().unwrap_or(silence));
-                    }
-                }
-                out.end_write();
-            }
-        };
-
-        let underflow_callback = {
-            let event_sender = self.event_sender.clone();
-            move || {
-                event_sender
-                    .send(InternalEvent::Underflow)
-                    .expect("Failed to send AudioOutputEvent::Underflow")
-            }
-        };
-
-        let error_callback = {
-            let event_sender = self.event_sender.clone();
-            move |err| {
-                event_sender
-                    .send(InternalEvent::Error(err))
-                    .expect("Failed to send AudioOutputEvent::Error")
-            }
-        };
-
-        let device = self.ctx.default_output_device()?;
-
-        let mut stream = {
+        let config = {
             let source = source.lock().expect("Failed to acquire audio source lock");
-            let sample_rate = source
-                .sample_rate()
-                .try_into()
-                .expect("Invalid sample rate");
-            let channels = source.channels().into();
-            device.open_outstream(
-                sample_rate,
-                soundio::Format::S16LE,
-                soundio::ChannelLayout::get_default(channels),
-                WRITE_CALLBACK_LATENCY_SEC,
-                write_callback,
-                Some(underflow_callback),
-                Some(error_callback),
-            )?
+            let mut config = DeviceConfig::new(DeviceType::Playback);
+            config.playback_mut().set_format(Format::S16);
+            config.playback_mut().set_channels(source.channels().into());
+            config.set_sample_rate(source.sample_rate());
+            config
         };
 
-        stream.start()?;
+        let mut device = Device::new(None, &config)?;
+        let mut started = false;
 
-        loop {
-            for event in self.event_receiver.try_iter() {
-                match event {
-                    InternalEvent::Error(err) => {
-                        log::error!("audio output error: {}", err);
-                        return Err(err.into());
+        dbg!(device.playback().format());
+
+        device.set_data_callback(move |_device, output, _frames| {
+            let mut source = source.lock().expect("Failed to acquire audio source lock");
+            for sample in output.as_samples_mut() {
+                *sample = source.next().unwrap_or(0);
+            }
+        });
+
+        for event in self.event_receiver.iter() {
+            match event {
+                InternalEvent::Close => {
+                    log::debug!("closing audio output");
+                    if started {
+                        device.stop()?;
                     }
-                    InternalEvent::Close => {
-                        log::debug!("closing audio output");
-                        return Ok(());
+                    break;
+                }
+                InternalEvent::Pause => {
+                    log::debug!("pausing audio output");
+                    if started {
+                        device.stop()?;
+                        started = false;
                     }
-                    InternalEvent::DevicesChanged => {
-                        log::info!("audio devices changed");
-                        // TODO:
-                        //  List devices and decide if we want to transfer.
-                    }
-                    InternalEvent::Underflow => {
-                        log::warn!("audio output underflow");
-                    }
-                    InternalEvent::Pause => {
-                        log::debug!("pausing audio output");
-                        stream
-                            .pause(true)
-                            .expect("Failed to pause the audio output");
-                    }
-                    InternalEvent::Resume => {
-                        log::debug!("resuming audio output");
-                        stream
-                            .pause(false)
-                            .expect("Failed to resume the audio output");
+                }
+                InternalEvent::Resume => {
+                    log::debug!("resuming audio output");
+                    if !started {
+                        device.start()?;
+                        started = true;
                     }
                 }
             }
-            self.ctx.wait_events();
         }
+
+        Ok(())
     }
 }
 
 enum InternalEvent {
-    Error(soundio::Error),
-    DevicesChanged,
-    Underflow,
     Close,
     Pause,
     Resume,
 }
 
-impl From<soundio::Error> for Error {
-    fn from(err: soundio::Error) -> Error {
+impl From<miniaudio::Error> for Error {
+    fn from(err: miniaudio::Error) -> Error {
         Error::AudioOutputError(Box::new(err))
     }
 }
