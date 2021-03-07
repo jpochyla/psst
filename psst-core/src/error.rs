0442f0a5af1a631382a0d166ddd4385851b7d777
Caden Haustein <code@brightlysalty.33mail.com>
2021-03-07 11:13:22 -0600

    Add more informative error messages

--- psst-core/src/error.rs
+++ psst-core/src/error.rs
@@ -21,7 +21,20 @@ impl fmt::Display for Error {
             Self::SessionDisconnected => write!(f, "Session disconnected"),
             Self::UnexpectedResponse => write!(f, "Unknown server response"),
             Self::AudioFileNotFound => write!(f, "Audio file not found"),
-            Self::AuthFailed { code } => write!(f, "Authentication failed: {code}", code = code),
+            Self::AuthFailed { code } => match code {
+                0 => write!(f, "Authentication failed: protocol error"),
+                2 => write!(f, "Authentication failed: try another AP"),
+                5 => write!(f, "Authentication failed: bad connection id"),
+                9 => write!(f, "Authentication failed: travel restriction"),
+                11 => write!(f, "Authentication failed: premium account required"),
+                12 => write!(f, "Authentication failed: bad credentials"),
+                13 => write!(f, "Authentication failed: could not validate credentials"),
+                14 => write!(f, "Authentication failed: account exists"),
+                15 => write!(f, "Authentication failed: extra verification required"),
+                16 => write!(f, "Authentication failed: invalid app key"),
+                17 => write!(f, "Authentication failed: application banned"),
+                _ => write!(f, "Authentication failed with error code {code}", code = code)
+            },
             Self::JsonError(err)
             | Self::AudioFetchingError(err)
             | Self::AudioDecodingError(err)
