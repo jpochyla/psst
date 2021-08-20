cfd83c2bdf407b71c2f242817889378e1cf8965f
Jan Pochyla <jpochyla@gmail.com>
2021-08-20 14:14:58 +0200

    core: Combine URL expiration parsing

--- psst-core/src/cdn.rs
+++ psst-core/src/cdn.rs
@@ -140,13 +140,24 @@ fn parse_total_content_length(response: &ureq::Response) -> u64 {
 }
 
 /// Parses an expiration of an audio file URL.
-/// Expiration is stored as `exp` field after `__token__`:
+/// Expiration is stored either as:
 ///
-/// .../...a35817ca410?__token__=exp=1629466995~hmac=df348...
-///                                  ^========^
+///  1. `exp` field after `__token__`:
+///     .../...a35817ca410?__token__=exp=1629466995~hmac=df348...
+///                                      ^========^
+///  2. or at the beginning of the first query parameter:
+///     .../59db919e18d6336461a0c71da051842ceef1b5af?1602319025_wu-SPeHxn...
+///                                                  ^========^
 fn parse_expiration(url: &str) -> Option<Duration> {
-    let token_exp = url.split("__token__=exp=").nth(1)?;
-    let expires_millis = token_exp.split('~').next()?;
+    let token_exp = url.split("__token__=exp=").nth(1);
+    let expires_millis = if let Some(token_exp) = token_exp {
+        // Parse from the expiration token param.
+        token_exp.split('~').next()?
+    } else {
+        // Parse from the first param.
+        let first_param = url.split('?').nth(1)?;
+        first_param.split('_').next()?
+    };
     let expires_millis = expires_millis.parse().ok()?;
     let expires = Duration::from_millis(expires_millis);
     Some(expires)
