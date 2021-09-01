966a620bb0cae33a2306bbb78985c5779962b42f
Eduardo Flores <edfloreshz@gmail.com>
2021-08-31 18:17:06 -0700

    Removed unnecessary match statement.

--- psst-gui/src/ui/theme.rs
+++ psst-gui/src/ui/theme.rs
@@ -1,6 +1,7 @@
 use druid::{Color, Env, FontDescriptor, FontFamily, FontWeight, Insets, Key, Size};
 #[cfg(any(target_os = "macos", target_os = "windows"))]
 use dark_light::{detect, Mode};
+#[cfg(target_os = "linux")]
 use detect_desktop_environment::DesktopEnvironment;
 pub use druid::theme::*;
 
@@ -60,30 +61,6 @@ pub fn setup(env: &mut Env, state: &AppState) {
     env.set(BACKGROUND_DARK, env.get(GREY_600));
     env.set(FOREGROUND_LIGHT, env.get(GREY_100));
     env.set(FOREGROUND_DARK, env.get(GREY_000));
-
-    match state.config.theme {
-        Theme::Light => {
-            env.set(BUTTON_LIGHT, env.get(GREY_700));
-            env.set(BUTTON_DARK, env.get(GREY_600));
-        }
-        Theme::Dark => {
-            env.set(BUTTON_LIGHT, env.get(GREY_600));
-            env.set(BUTTON_DARK, env.get(GREY_700));
-        }
-        Theme::Auto => {
-            #[cfg(any(target_os = "macos", target_os = "windows"))]
-            match detect() {
-                Mode::Dark => {
-                    env.set(BUTTON_LIGHT, env.get(GREY_600));
-                    env.set(BUTTON_DARK, env.get(GREY_700));
-                },
-                Mode::Light => {
-                    env.set(BUTTON_LIGHT, env.get(GREY_700));
-                    env.set(BUTTON_DARK, env.get(GREY_600));
-                }
-            }
-        }
-    }
 
     env.set(BORDER_LIGHT, env.get(GREY_400));
     env.set(BORDER_DARK, env.get(GREY_500));
@@ -145,12 +122,8 @@ pub fn setup(env: &mut Env, state: &AppState) {
 fn setup_auto_theme(env: &mut Env) {
     #[cfg(any(target_os = "macos", target_os = "windows"))]
     match detect() {
-        Mode::Dark => {
-            setup_dark_theme(env)
-        },
-        Mode::Light => {
-            setup_light_theme(env)
-        }
+        Mode::Dark => setup_dark_theme(env),
+        Mode::Light => setup_light_theme(env),
     }
     #[cfg(target_os = "linux")]
     match DesktopEnvironment::detect() {
@@ -190,6 +163,9 @@ fn setup_light_theme(env: &mut Env) {
     env.set(BLUE_100, Color::rgb8(0x5c, 0xc4, 0xff));
     env.set(BLUE_200, Color::rgb8(0x00, 0x8d, 0xdd));
 
+    env.set(BUTTON_LIGHT, env.get(GREY_700));
+    env.set(BUTTON_DARK, env.get(GREY_600));
+
     env.set(RED, Color::rgba8(0xEB, 0x57, 0x57, 0xFF));
 
     env.set(LINK_HOT_COLOR, Color::rgba(0.0, 0.0, 0.0, 0.06));
@@ -208,6 +184,9 @@ fn setup_dark_theme(env: &mut Env) {
     env.set(GREY_700, Color::grey8(0x28));
     env.set(BLUE_100, Color::rgb8(0x00, 0x8d, 0xdd));
     env.set(BLUE_200, Color::rgb8(0x5c, 0xc4, 0xff));
+
+    env.set(BUTTON_LIGHT, env.get(GREY_600));
+    env.set(BUTTON_DARK, env.get(GREY_700));
 
     env.set(RED, Color::rgba8(0xEB, 0x57, 0x57, 0xFF));
 
