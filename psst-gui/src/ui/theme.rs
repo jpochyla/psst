b6aa8cbcbcd66823829ddcf3ffdc78130066d663
Jan Pochyla <jpochyla@gmail.com>
2021-11-18 20:21:07 +0100

    gui: Make the minor labels more faded

--- psst-gui/src/ui/theme.rs
+++ psst-gui/src/ui/theme.rs
@@ -48,8 +48,8 @@ pub fn setup(env: &mut Env, state: &AppState) {
 
     env.set(WINDOW_BACKGROUND_COLOR, env.get(GREY_700));
     env.set(TEXT_COLOR, env.get(GREY_100));
-    env.set(ICON_COLOR, env.get(GREY_300));
-    env.set(PLACEHOLDER_COLOR, env.get(GREY_300));
+    env.set(ICON_COLOR, env.get(GREY_400));
+    env.set(PLACEHOLDER_COLOR, env.get(GREY_400));
     env.set(PRIMARY_LIGHT, env.get(BLUE_100));
     env.set(PRIMARY_DARK, env.get(BLUE_200));
 
