8e633a4452ed4d9c6bf4c22e3af70c40aa1676f3
Sky Brewer <jacobrewer@gmail.com>
2022-02-25 09:55:51 +0100

    very basic ico creation out of github actions

--- psst-gui/build.rs
+++ psst-gui/build.rs
@@ -1,8 +1,20 @@
 fn main() {
     #[cfg(windows)]
     {
+        build_logo_ico();
         let mut res = winres::WindowsResource::new();
         res.set_icon("assets/logo.ico");
         res.compile().expect("Could not attach exe icon");
     }
+}
+
+fn build_logo_ico() {
+    let mut icon_dir = ico::IconDir::new(ico::ResourceType::Icon);
+    // Read a PNG file from disk and add it to the collection:
+    let file = std::fs::File::open("assets/logo_256.png").unwrap();
+    let image = ico::IconImage::read_png(file).unwrap();
+    icon_dir.add_entry(ico::IconDirEntry::encode(&image).unwrap());
+    // Finally, write the ICO file to disk:
+    let file = std::fs::File::create("assets/logo.ico").unwrap();
+    icon_dir.write(file).unwrap();
 }
