f4454019767affb1035586f2fa5802dfae526d41
Jackson Goode <54308792+jacksongoode@users.noreply.github.com>
2025-05-06 23:04:31 +0900

    Make description consistent

--- .homebrew/generate_formula.sh
+++ .homebrew/generate_formula.sh
@@ -5,14 +5,10 @@ set -eo pipefail
 REPO_OWNER="jpochyla"
 REPO_NAME="psst"
 
-LATEST_VERSION_TAG_NO_V=$(git ls-remote --tags "https://github.com/${REPO_OWNER}/${REPO_NAME}.git" | \
-    grep -Eo 'refs/tags/v[0-9]{4}\.[0-9]{2}\.[0-9]{2}-[a-f0-9]{7}$' | \
-    sed 's|refs/tags/v||' | sort -V | tail -n1)
-
-if [ -z "$LATEST_VERSION_TAG_NO_V" ]; then
-  echo "Error: No versioned tag found." >&2
-  exit 1
-fi
+LATEST_VERSION_TAG_NO_V=$(git ls-remote --tags "https://github.com/${REPO_OWNER}/${REPO_NAME}.git" |
+	grep -Eo 'refs/tags/v[0-9]{4}\.[0-9]{2}\.[0-9]{2}-[a-f0-9]{7}$' |
+	sed 's|refs/tags/v||' | sort -V | tail -n1)
+: "${LATEST_VERSION_TAG_NO_V:?Error: No versioned tag found.}"
 
 VERSION="$LATEST_VERSION_TAG_NO_V"
 TAG_WITH_V="v${VERSION}"
@@ -20,23 +16,13 @@ TAG_WITH_V="v${VERSION}"
 RELEASE_INFO_JSON=$(curl -sL "https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/releases/tags/${TAG_WITH_V}")
 
 DMG_URL=$(echo "$RELEASE_INFO_JSON" | jq -r '.assets[] | select(.name=="Psst.dmg") | .browser_download_url')
-CHECKSUMS_URL=$(echo "$RELEASE_INFO_JSON" | jq -r '.assets[] | select(.name=="checksums.txt") | .browser_download_url')
+: "${DMG_URL:?Error: Could not find Psst.dmg asset URL for tag ${TAG_WITH_V}.}"
 
-if [ -z "$DMG_URL" ] || [ "$DMG_URL" == "null" ]; then
-  echo "Error: Could not find Psst.dmg asset URL for tag ${TAG_WITH_V}." >&2
-  exit 1
-fi
-if [ -z "$CHECKSUMS_URL" ] || [ "$CHECKSUMS_URL" == "null" ]; then
-  echo "Error: Could not find checksums.txt asset URL for tag ${TAG_WITH_V}." >&2
-  exit 1
-fi
+CHECKSUMS_URL=$(echo "$RELEASE_INFO_JSON" | jq -r '.assets[] | select(.name=="checksums.txt") | .browser_download_url')
+: "${CHECKSUMS_URL:?Error: Could not find checksums.txt asset URL for tag ${TAG_WITH_V}.}"
 
 SHA256=$(curl -sL "$CHECKSUMS_URL" | grep '\./Psst.dmg/Psst.dmg$' | awk '{print $1}')
-
-if [ -z "$SHA256" ]; then
-  echo "Error: Could not find SHA256 for Psst.dmg in checksums.txt." >&2
-  exit 1
-fi
+: "${SHA256:?Error: Could not find SHA256 for Psst.dmg in checksums.txt.}"
 
 cat <<EOF
 cask "psst" do
@@ -46,7 +32,7 @@ cask "psst" do
   url "${DMG_URL}",
       verified: "github.com/${REPO_OWNER}/${REPO_NAME}/"
   name "Psst"
-  desc "Fast, native Spotify client"
+  desc "Fast and native Spotify client"
   homepage "https://github.com/${REPO_OWNER}/${REPO_NAME}/"
 
   livecheck do
