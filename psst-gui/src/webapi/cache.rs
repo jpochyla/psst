ae663b5862069f4bbe00b9b9f69265e1c3b456d5
Jackson Goode <54308792+jacksongoode@users.noreply.github.com>
2025-11-22 19:26:12 +0900

    Import

--- psst-gui/src/webapi/cache.rs
+++ psst-gui/src/webapi/cache.rs
@@ -13,6 +13,8 @@ use lru::LruCache;
 use parking_lot::Mutex;
 use psst_core::cache::mkdir_if_not_exists;
 
+use crate::data::utils::crop_to_square;
+
 pub struct WebApiCache {
     base: Option<PathBuf>,
     images: Mutex<LruCache<Arc<str>, ImageBuf>>,
@@ -40,7 +42,7 @@ impl WebApiCache {
         self.key("images", &format!("{hash:016x}"))
             .and_then(|path| std::fs::read(path).ok())
             .and_then(|bytes| image::load_from_memory(&bytes).ok())
-            .map(crate::data::utils::crop_to_square)
+            .map(crop_to_square)
             .map(ImageBuf::from_dynamic_image)
     }
 
