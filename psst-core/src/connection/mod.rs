96863cf4530ccd351476fd7d7d88d6c4142be239
grenaad <dan4rie@gmail.com>
2026-02-21 15:52:58 +0200

    fixed

--- psst-core/src/connection/mod.rs
+++ psst-core/src/connection/mod.rs
@@ -429,7 +429,7 @@ fn compute_keys(
 
 fn client_response_encrypted(credentials: Credentials) -> ShannonMsg {
     use librespot_protocol::authentication::{
-        ClientResponseEncrypted, LoginCredentials, SystemInfo, Os, CpuFamily
+        ClientResponseEncrypted, CpuFamily, LoginCredentials, Os, SystemInfo,
     };
 
     let response = ClientResponseEncrypted {
