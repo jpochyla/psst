f205b1475fe641985f3ad3ae60fda4125ef438db
grenaad <dan4rie@gmail.com>
2026-02-27 14:03:52 +0200

    review update

--- psst-gui/src/data/promise.rs
+++ psst-gui/src/data/promise.rs
@@ -6,17 +6,9 @@ use crate::error::Error;
 pub enum Promise<T: Data, D: Data = (), E: Data = Error> {
     #[default]
     Empty,
-    Deferred {
-        def: D,
-    },
-    Resolved {
-        def: D,
-        val: T,
-    },
-    Rejected {
-        def: D,
-        err: E,
-    },
+    Deferred { def: D },
+    Resolved { def: D, val: T },
+    Rejected { def: D, err: E },
 }
 
 #[derive(Eq, PartialEq, Debug)]
@@ -120,3 +112,5 @@ impl<D: Data + Default, T: Data, E: Data> Promise<T, D, E> {
         *self = Self::Deferred { def: D::default() };
     }
 }
+
+
